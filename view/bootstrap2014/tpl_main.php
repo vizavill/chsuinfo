b44ba5a1cf2f0b51728489b8db7bd93d1d7c8014
Alexey Duryagin <aidalexeyaid@mail.ru>
2014-10-19 16:18:12 +0400

    meta теги

--- view/bootstrap2014/tpl_main.php
+++ view/bootstrap2014/tpl_main.php
@@ -2,8 +2,13 @@
 <html lang="ru">
   <head>
     <meta charset="utf-8">
+	<meta name="description" content="Расписание ЧГУ. Расписание занятий и экзаменов для студентов Череповецкого Государственного Университета." />
+    <meta name="keywords" content="Чгу, Череповецкий Государственный Университет, Расписание, Экзамены" />
+    <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+	
     <title><?=$title?></title>
 
     <!-- Bootstrap -->
